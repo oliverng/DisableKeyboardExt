fcac0ef113a3373e6cfff7fe7e896bedc17e9e41
Oliver N <oliverng@gmail.com>
2015-06-02 23:50:18 +0800

    First
    Quick test to disable keyboard extensions.

--- DisableKeyboardExt/ViewController.m
+++ DisableKeyboardExt/ViewController.m
@@ -2,7 +2,7 @@
 //  ViewController.m
 //  DisableKeyboardExt
 //
-//  Created by Oliver and Vienne Ng on 2/6/15.
+//  Created by Oliver Ng on 2/6/15.
 //  Copyright (c) 2015 ONG. All rights reserved.
 //
 
@@ -18,6 +18,17 @@
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
 }
+
+// This code is based upon Apple documentation found for IOS8 below
+// https://developer.apple.com/library/ios/documentation/UIKit/Reference/UIApplicationDelegate_Protocol/index.html#//apple_ref/occ/intfm/UIApplicationDelegate/application:shouldAllowExtensionPointIdentifier:
+// implementing this function overrides default custom keyboard behavior for higher security requirements
+- (BOOL)application:(UIApplication *)application shouldAllowExtensionPointIdentifier:(NSString *)extensionPointIdentifier {
+  if ([extensionPointIdentifier isEqualToString: UIApplicationKeyboardExtensionPointIdentifier]) {
+    return NO;
+  }
+  return YES;
+}
+
 
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
