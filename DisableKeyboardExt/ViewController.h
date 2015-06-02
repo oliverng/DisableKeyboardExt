55c8829bb3bde87adcb74ef261e853221ea96e45
Oliver N <oliverng@gmail.com>
2015-06-02 23:37:01 +0800

    Initial Commit

---
+++ DisableKeyboardExt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DisableKeyboardExt
+//
+//  Created by Oliver and Vienne Ng on 2/6/15.
+//  Copyright (c) 2015 ONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DisableKeyboardExtTests/DisableKeyboardExtTests.m
@@ -0,0 +1,40 @@
+//
+//  DisableKeyboardExtTests.m
+//  DisableKeyboardExtTests
+//
+//  Created by Oliver and Vienne Ng on 2/6/15.
+//  Copyright (c) 2015 ONG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface DisableKeyboardExtTests : XCTestCase
+
+@end
+
+@implementation DisableKeyboardExtTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
